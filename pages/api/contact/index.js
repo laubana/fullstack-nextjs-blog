402b765b4a71515726c2fa4a9d096adcc67c0f62
yban <laubana@gmail.com>
2025-01-31 10:43:52 -0800

    change readme

--- pages/api/contact/index.js
+++ pages/api/contact/index.js
@@ -28,7 +28,7 @@ const handler = async (req, res) => {
   } catch (error) {
     console.error(error);
 
-    res.status(500).json({ message: "Server Error!" });
+    res.status(500).json({ message: "Server Error" });
   }
 };
 
